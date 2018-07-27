9e9944e089d50d587360100562a85183173c072e
NicklessP <41351667+NicklessP@users.noreply.github.com>
2018-07-27 10:13:07 +1000

    MsgBox appears after inputing task

--- Form2.vb
+++ Form2.vb
@@ -1,4 +1,9 @@
 Public Class Form2
+    Dim difficulty As Integer
+    Dim time As Integer
+    Dim MHD As String
+    Dim task As String
+    Dim name As String
     Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
 
     End Sub
@@ -9,29 +14,46 @@ Public Class Form2
 
     Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
         HScrollBar1.Maximum = 33
-        HScrollBar1.Value = 24
+        HScrollBar1.Value = 1
         Label5.Text = HScrollBar1.Value
+        MHD = "Hours"
     End Sub
 
     Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
         HScrollBar1.Maximum = 99
-        HScrollBar1.Value = 90
+        HScrollBar1.Value = 1
         Label5.Text = HScrollBar1.Value
+        MHD = "Minutes"
     End Sub
 
     Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
         HScrollBar1.Maximum = 23
-        HScrollBar1.Value = 14
+        HScrollBar1.Value = 1
         Label5.Text = HScrollBar1.Value
+        MHD = "Days"
     End Sub
 
     Private Sub HScrollBar2_Scroll(sender As Object, e As ScrollEventArgs) Handles HScrollBar2.Scroll
         Label6.Text = HScrollBar2.Value
     End Sub
 
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        HScrollBar1.Value = 1
+        HScrollBar2.Value = 1
+        Label6.Text = HScrollBar2.Value
+        Label5.Text = HScrollBar1.Value
+    End Sub
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        difficulty = HScrollBar2.Value
+        time = HScrollBar1.Value
+        name = TextBox1.Text
         Me.Close()
-        'Bring Experience points from Form 2 back to Form 1
-        Form1.pgbExprienceBar.Value = 10
+        MsgBox("Task: " & name & vbNewLine & "Difficulty: " & difficulty & vbNewLine & "Length: " & time & " " & MHD & vbNewLine & "Type: " & task)
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        task = ComboBox1.Text
     End Sub
 End Class
