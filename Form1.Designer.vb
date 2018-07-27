e5054dfe57095ba25dfea0870ef088c0ffc7cfef
Tretho <41351608+Tretho@users.noreply.github.com>
2018-07-27 10:03:19 +1000

    Update Form1.Designer.vb

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,4 +1,4 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
@@ -23,6 +23,9 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btn_Task_Create = New System.Windows.Forms.Button()
+        Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.pgbExprienceBar = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'btn_Task_Create
@@ -34,17 +37,45 @@ Partial Class Form1
         Me.btn_Task_Create.Text = "Create Task"
         Me.btn_Task_Create.UseVisualStyleBackColor = True
         '
+        'PrintDialog1
+        '
+        Me.PrintDialog1.UseEXDialog = True
+        '
+        'pgbExprienceBar
+        '
+        Me.pgbExprienceBar.Location = New System.Drawing.Point(50, 213)
+        Me.pgbExprienceBar.Minimum = 1
+        Me.pgbExprienceBar.Name = "pgbExprienceBar"
+        Me.pgbExprienceBar.Size = New System.Drawing.Size(243, 23)
+        Me.pgbExprienceBar.TabIndex = 1
+        Me.pgbExprienceBar.Value = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 194)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Label1"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(656, 308)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.pgbExprienceBar)
         Me.Controls.Add(Me.btn_Task_Create)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents btn_Task_Create As Button
+    Friend WithEvents PrintDialog1 As PrintDialog
+    Friend WithEvents pgbExprienceBar As ProgressBar
+    Friend WithEvents Label1 As Label
 End Class
