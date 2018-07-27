663abd6628e1d092f1a8c05fda27003ccb37dd18
NicklessP <41351667+NicklessP@users.noreply.github.com>
2018-07-27 16:31:24 +1000

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,5 @@
 Public Class Form1
-
+    Dim GrossXP As Integer
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -9,9 +9,34 @@ Public Class Form1
     End Sub
 
     Private Sub ExpreienceBar_Click(sender As Object, e As EventArgs) Handles pgbExprienceBar.Click
-        Dim x As Form2 = New Form2
-        x.Show()
+
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        ListBox1.Items.Clear()
+        CheckBox1.Enabled = False
+        CheckBox1.CheckState = False
+        GrossXP += Form2.EXP1
+        Label1.Text = Form2.EXP1
+
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        ListBox2.Items.Clear()
+        CheckBox2.Enabled = False
+        CheckBox2.CheckState = False
+        GrossXP += Form2.EXP2
+        Label1.Text = GrossXP
+
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
+        ListBox3.Items.Clear()
+        CheckBox3.Enabled = False
+        CheckBox3.CheckState = False
+        GrossXP += Form2.EXP3
+        Label1.Text = GrossXP
 
     End Sub
 End Class
-
