25698dcf5744e5029d9cf3aaf86cc0890e5de98f
Tretho <41351608+Tretho@users.noreply.github.com>
2018-07-27 10:02:02 +1000

    Update Form 1

--- Form1.vb
+++ Form1.vb
@@ -1,4 +1,4 @@
-﻿Public Class Form1
+Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -7,4 +7,11 @@
     Private Sub btn_Task_Create_Click(sender As Object, e As EventArgs) Handles btn_Task_Create.Click
         Form2.Show()
     End Sub
+
+    Private Sub ExpreienceBar_Click(sender As Object, e As EventArgs) Handles pgbExprienceBar.Click
+        Dim x As Form2 = New Form2
+        x.Show()
+
+    End Sub
 End Class
+
