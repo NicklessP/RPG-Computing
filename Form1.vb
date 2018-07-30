658548d80c3f2e578962e4a07a829f75b694c60c
NicklessP <41351667+NicklessP@users.noreply.github.com>
2018-07-30 12:46:58 +1000

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,6 @@
 Public Class Form1
-    Dim GrossXP As Integer
+    Dim GrossXP, LevelUp As Integer
+    Public EXP1, EXP2, EXP3 As Integer
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -14,29 +15,49 @@ Public Class Form1
     End Sub
 
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
         ListBox1.Items.Clear()
         CheckBox1.Enabled = False
         CheckBox1.CheckState = False
-        GrossXP += Form2.EXP1
-        Label1.Text = Form2.EXP1
+        GrossXP += EXP1
+        LevelUp += EXP1
+        If LevelUp >= 100 Then
+            LevelUp = 0
+            pgbExprienceBar.Value = 0
+        End If
+        pgbExprienceBar.Value += EXP1
+        Label1.Text = pgbExprienceBar.Value
 
     End Sub
 
     Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
-        ListBox2.Items.Clear()
-        CheckBox2.Enabled = False
-        CheckBox2.CheckState = False
-        GrossXP += Form2.EXP2
-        Label1.Text = GrossXP
+
+        ListBox1.Items.Clear()
+        CheckBox1.Enabled = False
+        CheckBox1.CheckState = False
+        GrossXP += EXP2
+
+        pgbExprienceBar.Value += EXP2
+        If pgbExprienceBar.Value >= 100 Then
+            pgbExprienceBar.Value = 0
+        End If
+        Label1.Text = pgbExprienceBar.Value
+
 
     End Sub
 
     Private Sub CheckBox3_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox3.CheckedChanged
-        ListBox3.Items.Clear()
-        CheckBox3.Enabled = False
-        CheckBox3.CheckState = False
-        GrossXP += Form2.EXP3
-        Label1.Text = GrossXP
+
+        ListBox1.Items.Clear()
+        CheckBox1.Enabled = False
+        CheckBox1.CheckState = False
+        GrossXP += EXP3
+
+        pgbExprienceBar.Value += EXP3
+        If pgbExprienceBar.Value >= 100 Then
+            pgbExprienceBar.Value = 0
+        End If
+        Label1.Text = pgbExprienceBar.Value
 
     End Sub
 End Class
